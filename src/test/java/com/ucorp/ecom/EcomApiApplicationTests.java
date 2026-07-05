0eba0ef5aeeb6d2e659610708c201913295c5c4c
fadli <fadlilatul.umamm@gmail.com>
2026-07-05 20:15:35 +0700

    Feat : Setup initial Java Spring Boot Project files

---
+++ src/test/java/com/ucorp/ecom/EcomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucorp.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
